8c17f4c47716accc799f4b69cbd3435441ca7459
Gianna Besmonte <gbesmont@usc.edu>
2024-04-14 17:25:01 -0700

    problem now is that alphabet can't do two letters or more

--- wordle.cpp
+++ wordle.cpp
@@ -17,8 +17,8 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-void solve(const std::string& in, const std::string& floating, std::string currStr, std::deque<char> pool, std::set<std::string>& s);
-
+void solve(const std::string& in, const std::string& floating, std::string currStr, std::string pool, std::set<std::string>& s);
+void solve2(const std::string& in, const std::string& floating, std::string currStr, std::string pool, std::set<std::string>& s);
 // Definition of primary wordle function
 std::set<std::string> wordle(
         const std::string& in,
@@ -28,25 +28,20 @@ std::set<std::string> wordle(
     // Add your code here
     std::string currStr = in;
     std::set<std::string> s;
-    std::deque<char> pool;
     std::set<string>::iterator it;
     std::set<string>::iterator it2;
     std::set<std::string> s2;
     std::string alphabet = "abcdefghijklmnopqrstuvwxyz";
     std::set<std::string> s3;
-    for (int i = 0; i < floating.length(); i++){
-        pool.push_back(floating[i]);
-    }
-    solve(in, floating, currStr, pool, s);
-    for (int i = 0; i < alphabet.length(); i++){
-        pool.push_back(alphabet[i]);
-    }
+    solve(in, floating, currStr, floating, s);
     for (it = s.begin(); it != s.end(); ++it){
         //cout << *it << endl;
         currStr = *it;
-        solve(*it, alphabet, currStr, pool, s2);
+        solve(*it, alphabet, currStr, alphabet, s2);
     }
+    //cout << "DIVIDER" << endl;
     for (it2 = s2.begin(); it2 != s2.end(); ++it2){
+        //cout << *it2 << endl;
         if (dict.find(*it2) != dict.end()) {
             s3.insert(*it2);
         }
@@ -55,7 +50,7 @@ std::set<std::string> wordle(
 }
 
 // Define any helper functions here
-void solve(const std::string& in, const std::string& floating, std::string currStr, std::deque<char> pool, std::set<std::string>& s){
+void solve(const std::string& in, const std::string& floating, std::string currStr, std::string pool, std::set<std::string>& s){
     //Check if word is full
     int a = 0;
     for (int i = 0; i < currStr.length(); i++){
@@ -71,28 +66,27 @@ void solve(const std::string& in, const std::string& floating, std::string currS
         return;
     }
     //Check if pool is empty
-    if (pool.empty()){
+    if (pool.length() == 0){
         s.insert(currStr);
         return;
     }
     int count = 0;
-    while (!pool.empty()){
+    while (pool.length() != 0){
         if (count == floating.length()){
             break;
         }
         for (int j = 0; j < currStr.length(); j++){
             if (currStr[j] == '-'){
-                currStr[j] = pool.back();
-                std::deque<char> newPool = pool;
-                newPool.pop_back();
+                currStr[j] = pool[pool.length()-1];
+                std::string newPool = pool.substr(0, pool.length()-1);
                 solve(in, floating, currStr, newPool, s);
                 currStr[j] = '-';
             }
         }
         count++;
-        char letter = pool.back();
-        pool.pop_back();
-        pool.push_front(letter);
+        char letter = pool[pool.length()-1];
+        pool = pool.substr(0, pool.length()-1);
+        pool = letter + pool;
     }
 }
 
